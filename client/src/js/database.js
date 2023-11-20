6ce10139545e536474b0c50fb7b94c4afa9c2304
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-11-20 00:29:46 -0700

    adding methods to get all content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,44 +13,25 @@ const initdb = async () =>
   });
 
 // adds logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  console.error('putDb not implemented');
-  // Creates a connection to the database and version we want to use
+export const putDb = async (content) => {console.error('putDb not implemented');
+  // connections to database and version
   const textDb = await openDB("jate", 1);
-
-  // Creates a new transaction and specify the database and data privileges
   const tx = textDb.transaction("jate", "readwrite");
-
-  // Opens up the desired object store
   const store = tx.objectStore("jate");
-
-  // The .put() method is used on the store and content passed in
   const request = store.put({ id: 1, value: content });
-
-  // Confirmation of the request
   const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  console.log("data saved to the database", result);
 };
 
 // adds logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented'); {
-  // Creates a connection to the database and version we want to use
+export const getDb = async () => {console.error('getDb not implemented'); 
   const textDb = await openDB("jate", 1);
-
-  // Creates a new transaction and specify the database and data privileges
   const tx = textDb.transaction("jate", "readonly");
-
-  // Opens up the desired object store
   const store = tx.objectStore("jate");
-
-  // The .get() method is used on the store to grab stored data
   const request = store.get(1);
-
-  // Confirmation of the request
   const result = await request;
   console.log("result.value", result);
-
   return result?.value;
-}
+};
 
 initdb();
